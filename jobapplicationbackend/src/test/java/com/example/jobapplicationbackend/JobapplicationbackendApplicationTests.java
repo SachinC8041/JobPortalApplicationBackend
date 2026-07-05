5d0b8c4f1998c75abdb4d403eb9855306109de3e
SachinC8041 <sachinchole619@gmail.com>
2026-07-05 18:29:13 +0530

    Rebuild job portal backend

---
+++ jobapplicationbackend/src/test/java/com/example/jobapplicationbackend/JobapplicationbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapplicationbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
